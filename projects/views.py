ec4ae52e919e6ce0084fd8370578c21df51e3289
Tudor Prozorovschi <prozorovschitudor@gmail.com>
2024-11-12 02:26:28 +0200

    Actualizare proiect Django: descriere

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
+from django.shortcuts import render
  
 
 
@@ -14,14 +15,14 @@ def project(request, pk):
     projectobj = Project.objects.get(id=pk)  # Obținem proiectul pe baza ID-ului
     tags = projectobj.tags.all()  # Accesăm toate tag-urile asociate proiectului
     print('projectobj:', projectobj)
-    return render(request, 'single-project.html', {'project': projectobj, 'tags': tags})
+    return render(request, 'projects/single-project.html', {'project': projectobj, 'tags': tags})
 
 
 
 def createProject(request):
    form = ProjectForm()
    if request.method == 'POST':
-       form = ProjectForm(request.POST)
+       form = ProjectForm(request.POST, request.FILES)
        if form.is_valid():
            form.save()
            return redirect('projects')
@@ -34,8 +35,9 @@ def createProject(request):
 def updateProject(request, pk):
    project = Project.objects.get(id=pk)
    form = ProjectForm(instance=project)
+
    if request.method == 'POST':
-       form = ProjectForm(request.POST, instance=project)
+       form = ProjectForm(request.POST, request.FILES, instance=project)
        if form.is_valid():
            form.save()
            return redirect('projects')
@@ -54,4 +56,6 @@ def deleteProject(request, pk):
     context = {'object': project}
     return render(request, 'projects/delete_template.html', context)
 
+def home(request):
+    return render(request,'home.html')
 
