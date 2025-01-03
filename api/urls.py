441b29d8b4fdfdd138d21f4c5e16219e4ec42b91
Tudor Prozorovschi <prozorovschitudor@gmail.com>
2025-01-03 14:49:04 +0200

    add serializers and views for Project, Tag, Review, and Profile models; implement project retrieval endpoints

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
 
-path('', views.getRoutes)
+path('', views.getRoutes),
+path('projects/', views.getProjects),
+path('projects/<str:pk>/', views.getProject),
 
 
 ]
