ba37ff565c58e6b99eea700c381529cefbbf25b2
Tudor Prozorovschi <prozorovschitudor@gmail.com>
2025-01-04 18:26:17 +0200

    add JWT and token blacklist functionality; include migrations, models, and API endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -2,31 +2,35 @@ from rest_framework import serializers
 from projects.models import Project, Tag, Review
 from users.models import Profile
 
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
 
+
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
 
+
 class ProjectSerializer(serializers.ModelSerializer):
-    owner = ProfileSerializer(many=False)  # Relație One-to-One sau ForeignKey
-    tags = TagSerializer(many=True)       # Relație Many-to-Many
-    reviews = serializers.SerializerMethodField()  # Câmp personalizat pentru reviews
+    owner = ProfileSerializer(many=False)
+    tags = TagSerializer(many=True)
+    reviews = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
         fields = '__all__'
 
     def get_reviews(self, obj):
-        reviews = obj.review_set.all()  # Accesează toate obiectele Review asociate cu proiectul
+        reviews = obj.review_set.all()
         serializer = ReviewSerializer(reviews, many=True)
         return serializer.data
--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getRoutes(request):
     routes = [
         {'GET': '/api/projects'},
@@ -25,8 +27,29 @@ def getProjects(request):
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
-def getProject(request,pk):
+def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
